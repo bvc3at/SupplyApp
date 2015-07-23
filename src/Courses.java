13cdcddc2d0793a02766494aa1df32ee8b5e0d1d
bvc3at <extazi910@gmail.com>
2015-07-24 00:49:47 +0300

    Add new course

--- src/Courses.java
+++ src/Courses.java
@@ -1,8 +1,6 @@
-/**
- * Created by Dmitry on 19.07.2015.
- */
+
 public enum Courses {
-    c090301("09.03.01"), c090302("09.03.02"), c090303("09.03.03"), c090304("09.03.04");
+    c090301("09.03.01"), c090302("09.03.02"), c090303("09.03.03"), c090304("09.03.04"), c010302("01.03.02");
     final String course;
     Courses(final String course) {
         this.course = course;
